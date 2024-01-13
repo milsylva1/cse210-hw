8e389d141dcf2e742c6ba57100318730ffcfc3c3
milsylva1 <sonsonblack@gmail.com>
2024-01-12 21:13:50 -0700

    prep

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+namespace prep1
+{
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {last}, {first}, {last}");
     }
+}
 }
